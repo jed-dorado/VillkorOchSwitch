f013f22457ec8da4356d870f9d4593b9a77edd74
jed <jed@telia.com>
2026-08-25 13:21:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Console.Write("Ange din ålder");
+//int age = int.Parse(Console.ReadLine());
+
+//if (age >= 18)
+//{
+//    Console.WriteLine("Du är myndig.");
+//}
+//else
+//{
+//    Console.WriteLine("Du är inte myndig.");
+//}
+//Betygs godkännande
+//Console.Write("Ange betyg (A-F): ");
+//string grade = Console.ReadLine().ToUpper();
+//    if (grade== "F")
+//    {
+//        Console.WriteLine("Ej godkänd");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Godkänd");
+//    }
+
+
+Console.Write("Skriv temperaturen i Celsius: ");
+int temp = int.Parse(Console.ReadLine());
+
+switch (temp)
+    {
+    case int n when (n <= 0):
+        Console.WriteLine("Det är frost");
+        break;
+    case int n when (n >= 0 && n <= 20):
+        Console.WriteLine("Lite kyligt");
+        break;
+    case int n when (n >= 20):
+        Console.WriteLine("Det är varmt");
+        break;
+
+}
